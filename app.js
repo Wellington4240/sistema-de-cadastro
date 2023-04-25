820580cdd3a264d27adcf7632ed2109ed5be1206
Wellington4240 <116130782+Wellington4240@users.noreply.github.com>
2023-04-25 10:21:23 -0300

    Indicando modificações do paulo

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
-
+//Mais um console log de teste:
+console.log("O Paulo fez esse commit sem você ver na aula!")
